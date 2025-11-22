896a2e551b78fcbce29b86b77ef37b454e11cf79
sudo-0-AM <yaduvanshi.piyush.9a@gmail.com>
2025-11-22 23:04:15 +0530

    fix: add ethers dependency for Vercel

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/',
-})
+export default {
+  optimizeDeps: {
+    include: ["ethers"],
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+};
