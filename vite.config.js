45d7b5cde21a9266386ad5363343aba8599bae68
sudo-0-AM <yaduvanshi.piyush.9a@gmail.com>
2025-11-22 23:13:17 +0530

    fix: add ethers dependency for netlify

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default {
-  optimizeDeps: {
-    include: ["ethers"],
-  },
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-  },
-};
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
+})
